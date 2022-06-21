a80437b4b24ba8f1672d7ccbf4ad6738a0c32e8c
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2022-06-21 20:05:53 -0300

    alterei o script ruby auxiliar para usar params

--- csv2json.rb
+++ csv2json.rb
@@ -1,9 +1,10 @@
 require 'csv'
 require 'json'
 
-arq = "/tmp/meu_pgd.csv"
+arq = ARGV[0]
+saida = ARGV[1]
 csv = CSV.open(arq, headers: true, header_converters: :symbol, col_sep: ';').map(&:to_h)
 
-f = File.open("/tmp/pgd.json", 'w')
+f = File.open(saida, 'w')
 f.write(csv.to_json)
 f.close
