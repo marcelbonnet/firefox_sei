187e052a9e63e115f774658df748b53de8bc7a0f
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2022-08-03 10:28:58 -0300

    Mantém a conexão ativa no Marval

--- js/marval_content.js
+++ js/marval_content.js
@@ -1,5 +1,33 @@
 // Adiciona elementos na página do Marval
 
+
+
+
+/* ********************************
+*	Mantém a conexão
+***********************************/
+setInterval(async function(){
+	// document.querySelector("button[identifier]").dispatchEvent(new Event('click'))
+	await fetch("https://visao.anatel.gov.br/MSM/ScriptHandler.ashx?method=GetWorklist&classPath=%2FMSM%2FAjax%2FAjaxService.asmx&classMode=undefined", {
+	    "credentials": "include",
+	    "headers": {
+	        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+	        "Accept": "application/json, text/javascript, */*",
+	        "Accept-Language": "en-US,en;q=0.5",
+	        "Content-Type": "application/x-www-form-urlencoded",
+	        "X-Requested-With": "XMLHttpRequest",
+	        "Sec-Fetch-Dest": "empty",
+	        "Sec-Fetch-Mode": "cors",
+	        "Sec-Fetch-Site": "same-origin"
+	    },
+	    "referrer": "https://visao.anatel.gov.br/MSM/default.aspx",
+	    "body": "numberOfRequests=5",
+	    "method": "POST",
+	    "mode": "cors"
+	});
+}, 60000)
+
+
 /* ********************************
 *	Tela de login
 ***********************************/
