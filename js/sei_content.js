b430b74f519190d58300a52a792cefa0e143b098
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2022-09-27 18:38:04 -0300

    Conserta o preenchimento de senha do SEI4

--- js/sei_content.js
+++ js/sei_content.js
@@ -1,1 +1,15 @@
 // placeholder caso queira adicionar itens na página do SEI
+
+// descaga o design que quebrou o preenchimento automático de senha no SEI 4:
+let pwdSenha = document.getElementById('pwdSenha')
+if ( pwdSenha !== null ){
+	let span = document.createElement('span')
+	span.innerHTML = "<strong>Você pode voltar a usar a senha salva pelo navegador.<strong>"
+	document.getElementById('divUsuario').append(span)
+
+	let pwd = document.querySelector('input[type="password"')
+	pwd.addEventListener('change', function(event){
+		pwdSenha.value = pwd.value
+		pwdSenha.dispatchEvent(new Event('change'));
+	})
+}
